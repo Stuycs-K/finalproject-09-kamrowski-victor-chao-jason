f80026d59d18c6ee08cc9434f268e7315a27ed77
Victor Kamrowski <vkamrowski50@stuy.edu>
2025-05-28 10:15:18 -0400

    small edits

--- Main.java
+++ Main.java
@@ -11,13 +11,15 @@ public class Main{
   Padding.input = Padding.input.substring(0,Padding.input.length()-1);
   //System.out.println(Padding.input);
   String paddedStr = Padding.pad(Padding.input);
-   System.out.println(paddedStr);
+   //System.out.println(paddedStr);
   for(int x = 0;x<16;x++){
     Padding.hexStrings[x] = Padding.binaryStringtoInt(paddedStr.substring(32*x,32*(x+1)));
     //System.out.println(Padding.hexStrings[x]);
   }
 for(int x = 0;x<16;x++){
     Op_One.opOne(Op_One.A,Op_One.B,Op_One.C,Op_One.D,x,x+1);
+    System.out.println(Op_One.A + " " + Op_One.B + " " + Op_One.C + " " + Op_One.D);
+
 }
 System.out.println(Op_One.A + " " + Op_One.B + " " + Op_One.C + " " + Op_One.D);
 }
--- Op_One.java
+++ Op_One.java
@@ -1,11 +1,12 @@
 public class Op_One{
 
 public static long A = 0x01234567;
-public static long B = 0x89abcdef;
-public static long C = 0xfedcba98;
+public static long B = 0x89ABCDEF;
+public static long C = 0xFEDCBA98;
 public static long D = 0x76543210;
 
   public static long F(long b,long c, long d){
+    System.out.println(b&c);
     long newval = (b&c)|((~b)&d);
     return newval;
   }
@@ -16,11 +17,16 @@ public static long D = 0x76543210;
 
   public static void opOne(long a,long b,long c, long d,int hexStrInt,int kConstAndSInt){
     long F1 = F(b,c,d);
+    System.out.println(F1);
     long MA1 = modularAddition(a, F1,100000000);
+    System.out.println(MA1);
     long MA2 = modularAddition(Padding.hexStrings[hexStrInt], MA1, 100000000);
+    System.out.println(MA2);
     long MA3 = modularAddition(Utils.KConstants[kConstAndSInt-1], MA2, 100000000);
+    System.out.println(MA3);
     //left bit shift
     String binStr = Long.toBinaryString(MA3);
+    System.out.println(binStr);
     binStr = Padding.zerosPad(-1, 32, binStr);
     String newBin = "" + binStr.substring(Utils.SValues[kConstAndSInt-1]) + binStr.substring(0,Utils.SValues[kConstAndSInt-1]);
     long afterShift = Padding.binaryStringtoInt(newBin);
