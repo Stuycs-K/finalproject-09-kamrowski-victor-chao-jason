0d440882d077354fa789e002e78be207ae90f61b
Victor Kamrowski <vkamrowski50@stuy.edu>
2025-05-30 10:01:09 -0400

    deleting unnecesarry lines because too messy

--- Op_One.java
+++ Op_One.java
@@ -2,35 +2,19 @@ import java.math.BigInteger;
 
 public class Op_One {
 
-    // public static long A = 0x01234567;
-    // public static long B = 0x89ABCDEF;
-    // public static long C = 0xFEDCBA98;
-    // public static long D = 0x76543210;
-    public static void main(String[] args) {
-        F(B, C, D);
-    }
-
     public static BigInteger A = new BigInteger("67452301", 16);
     public static BigInteger B = new BigInteger("efcdab89", 16);
     public static BigInteger C = new BigInteger("98badcfe", 16);
     public static BigInteger D = new BigInteger("10325476", 16);
-    public static BigInteger intForModAdd = new BigInteger("100000000", 16);
-    public static BigInteger mask32 = new BigInteger("FFFFFFFF",16);
+
+    public static final BigInteger mod32 = new BigInteger("100000000", 16);
+    public static final int mask32 = new BigInteger("FFFFFFFF",16);
 
     public static BigInteger F(BigInteger b, BigInteger c, BigInteger d) {
-        // System.out.println(b.and(c));
-        // long newval = (b&c)|((~b)&d);
         BigInteger bAndC = b.and(c);
         BigInteger notB = b.not().and(mask32);
         BigInteger notBAndD = notB.and(d);
-        BigInteger result = bAndC.or(notBAndD).and(mask32);
-
-        System.out.println("for b and c: " + bAndC);
-        System.out.println("for b not: " + notB);
-        System.out.println("for b not and d: " + notBAndD);
-                System.out.println("full " + result);
-        // System.out.println(newval);
-        return result;
+        return bAndC.or(notBAndD).and(mask32);
     }
 
     public static BigInteger modularAddition(BigInteger X, BigInteger Y, BigInteger Z) {
@@ -44,13 +28,13 @@ public class Op_One {
     public static void opOne(BigInteger a, BigInteger b, BigInteger c, BigInteger d, int hexStrInt, int kConstAndSInt) {
         BigInteger F1 = F(b, c, d);
         System.out.println(F1);
-        BigInteger MA1 = modularAddition(a, F1, intForModAdd);
+        BigInteger MA1 = modularAddition(a, F1, mod32);
         System.out.println(MA1);
         BigInteger MA2 = modularAddition(Padding.hexStrings[hexStrInt], 
-                                         MA1, intForModAdd);
+                                         MA1, mod32);
         System.out.println(MA2);
         BigInteger MA3 = modularAddition(new BigInteger(Utils.KConstants[kConstAndSInt - 1], 16), 
-                                         MA2, intForModAdd);
+                                         MA2, mod32);
         System.out.println(MA3);
         
         // left bit shift
@@ -59,7 +43,7 @@ public class Op_One {
         binStr = Padding.zerosPad(-1, 32, binStr);
         String newBin = "" + binStr.substring(Utils.SValues[kConstAndSInt - 1]) + binStr.substring(0, Utils.SValues[kConstAndSInt - 1]);
         BigInteger afterShift = Padding.binaryStringtoInt(newBin);
-        BigInteger MA4 = modularAddition(b, afterShift, intForModAdd);
+        BigInteger MA4 = modularAddition(b, afterShift, mod32);
 
         A = d;
         B = MA4;
