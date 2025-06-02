a095e89e51dd34b42bd3d78a8e44805ea676289b
Victor Kamrowski <vkamrowski50@stuy.edu>
2025-06-01 22:44:40 -0400

    fixed op two. now works for first 32 iterations. will finish op 3 and 4

--- Main.java
+++ Main.java
@@ -24,23 +24,23 @@ public class Main {
             Op_One.opOne(Op_One.A, Op_One.B, Op_One.C, Op_One.D, x, x + 1);
 
             //if (x == 14 || x == 15) {
-                System.out.println("----- i=" + x + " -----");
-                System.out.println(Op_One.A.toString(16) + " " +
-                                    Op_One.B.toString(16) + " " +
-                                    Op_One.C.toString(16) + " " +
-                                    Op_One.D.toString(16) + "\n");
+              //  System.out.println("----- i=" + x + " -----");
+               // System.out.println(Op_One.A.toString(16) + " " +
+                //                    Op_One.B.toString(16) + " " +
+                //                    Op_One.C.toString(16) + " " +
+                  //                  Op_One.D.toString(16) + "\n");
            // }
         }
-        System.out.println(Op_One.A.toString(16) + " " + Op_One.B.toString(16) + " " + Op_One.C.toString(16) + " " + Op_One.D.toString(16));
+       // System.out.println(Op_One.A.toString(16) + " " + Op_One.B.toString(16) + " " + Op_One.C.toString(16) + " " + Op_One.D.toString(16));
 
-        // for (int x = 16; x < 32; x++) {
-        //     System.out.println("----- i=" + x + " -----");
-        //     Op_Two.opTwo(Op_One.A, Op_One.B, Op_One.C, Op_One.D, x - 16, x + 1);
-        //     System.out.println("A: " + Op_One.A.toString(16) + " " +
-        //                        "B: " + Op_One.B.toString(16) + " " +
-        //                        "C: " + Op_One.C.toString(16) + " " +
-        //                        "D: " + Op_One.D.toString(16) + "\n");
-        // }
+         for (int x = 16; x < 32; x++) {
+             System.out.println("----- i=" + x + " -----");
+             Op_Two.opTwo(Op_One.A, Op_One.B, Op_One.C, Op_One.D, x - 16, x + 1);
+             System.out.println("A: " + Op_One.A.toString(16) + " " +
+                                "B: " + Op_One.B.toString(16) + " " +
+                                "C: " + Op_One.C.toString(16) + " " +
+                                "D: " + Op_One.D.toString(16) + "\n");
+         }
         // for (int x = 32; x < 48; x++) {
         //     Op_Three.opThree(Op_One.A, Op_One.B, Op_One.C, Op_One.D, x - 32, x + 1);
         // }
--- Op_Two.java
+++ Op_Two.java
@@ -3,26 +3,31 @@ import java.math.BigInteger;
 public class Op_Two {
 
     public static BigInteger G(BigInteger b, BigInteger c, BigInteger d) {
-        BigInteger newval = (b.and(d)).or(c.and(d.not()));
-        return newval;
+        BigInteger bAndD = d.and(b);
+        BigInteger notD = d.not().and(Op_One.mask32);
+        BigInteger cAndNotD = c.and(notD);
+        return bAndD.or(cAndNotD).and(Op_One.mask32);
     }
 
     public static void opTwo(BigInteger a, BigInteger b, BigInteger c, BigInteger d, int hexStrInt, int kConstAndSInt) {
         BigInteger G1 = G(b, c, d);
-        BigInteger MA1 = Op_One.modularAddition(a, G1, Op_One.mod32);
-        BigInteger MA2 = Op_One.modularAddition(Padding.hexStrings[(hexStrInt * 5 + 1) % 16],
-                                                MA1, Op_One.mod32);
-        BigInteger MA3 = Op_One.modularAddition(new BigInteger(Utils.KConstants[kConstAndSInt - 1], 16),
-                                                MA2, Op_One.mod32);
-        // left bit shift
-        String binStr = MA3.toString(2);
-        binStr = Padding.pad(binStr);
-        String newBin = "" + binStr.substring(Utils.SValues[kConstAndSInt - 1]) + binStr.substring(0, Utils.SValues[kConstAndSInt - 1]);
-        BigInteger afterShift = Padding.binaryStringtoInt(newBin);
-        BigInteger MA4 = Op_One.modularAddition(b, afterShift, Op_One.mod32);
+        int gindex = (5*hexStrInt+1)%16;
+        
+        BigInteger MofG = Padding.hexStrings[gindex];
+        BigInteger KofI = new BigInteger(Utils.KConstants[kConstAndSInt-1],16);
+        int SofI = Utils.SValues[kConstAndSInt - 1];
+
+        // adding all tgt
+        BigInteger temp = Op_One.modularAddition(a, G1, MofG, KofI);
+
+        // rotation
+        BigInteger rotation = Op_One.leftRotate(temp, SofI);
+
+        // new b
+        BigInteger Bnew = b.add(rotation).and(Op_One.mask32);
 
         Op_One.A = d;
-        Op_One.B = MA4;
+        Op_One.B = Bnew;
         Op_One.C = b;
         Op_One.D = c;
     }
